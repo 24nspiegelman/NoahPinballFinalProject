af3c04c1e8ecf117562bff13383575bc2a200c1b
Noah Spiegelman <noahspiegelman05@gmail.com>
2023-05-15 17:28:30 -0700

    Ball Bounces off Walls and Now Working on Adding Stuff

--- src/Ball.java
+++ src/Ball.java
@@ -4,31 +4,31 @@ import java.awt.RenderingHints;
 
 public class Ball {
 
-    private double x;
-    private double y;
-    private double velX;
-    private double velY;
+    private double xPos;
+    private double yPos;
+    private double xVel;
+    private double yVel;
     private double gravity;
 
 
     public Ball(){
-        x = 0;
-        y = 0;
-        velX = 1;
-        velY = 1;
+        xPos = 0;
+        yPos = 0;
+        xVel = 1;
+        yVel = 1;
         gravity = 0.5;
     }
     public void moveBall() {
-        x = x + velX;
-        y = y + velY + gravity;
+        xPos = xPos + xVel;
+        yPos = yPos + yVel + gravity;
         bounce();
     }
     public void bounce(){
-        if (x == 560 || x == 0){
-            velX *= -1;
+        if (xPos == 560 || xPos == 0){
+            xVel *= -1;
         }
-        if (y == 0 || y == 717){
-            velY *= -1;
+        if (yPos == 0 || yPos == 717){
+            yVel *= -1;
         }
     }
 
@@ -36,21 +36,21 @@ public class Ball {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.fillOval((int) getX(), (int) getY(), 10, 10);
+        g2d.fillOval((int) getXPos(), (int) getYPos(), 10, 10);
     }
 
 
-    public double getX(){
-        return this.x;
+    public double getXPos(){
+        return this.xPos;
     }
-    public double getY(){
-        return this.y;
+    public double getYPos(){
+        return this.yPos;
     }
-    public double getVelX(){
-        return this.velX;
+    public double getXVel(){
+        return this.xVel;
     }
-    public double getVelY(){
-        return this.velY;
+    public double getYVel(){
+        return this.yVel;
     }
 
 }
